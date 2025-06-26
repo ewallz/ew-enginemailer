616a0a662a15eb31a233b71d34727ef2ba7de5ea
egv <archonic08@gmail.com>
2025-06-26 08:49:58 +0800

    Upload release 1.0.0

---
+++ includes/addons-loader.php
@@ -0,0 +1,9 @@
+<?php
+// Addons loader for EW Enginemailer
+
+function ew_enginemailer_display_addons() {
+    echo '<div class="wrap">';
+    echo '<h2>' . __('EW Enginemailer Add-ons', 'ew-enginemailer') . '</h2>';
+    echo '<p>' . __('Add-ons are coming soon! Have a feature request? Please submit your idea or request on our <a href="https://github.com/ewallsolutions/ew-enginemailer/issues" target="_blank">GitHub Issues page</a>.', 'ew-enginemailer') . '</p>';
+    echo '</div>';
+}
